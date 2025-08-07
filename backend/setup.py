8543aab7c0ee694fc0bc8128d3189ebb2b135856
Riko68 <52465420+Riko68@users.noreply.github.com>
2025-08-07 10:53:44 +0200

    Add files via upload

---
+++ backend/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rag_backend",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    python_requires='>=3.8',
+)
